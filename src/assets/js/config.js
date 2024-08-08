a46f42402543307aef8bc0f31898a8f3e5ffcff5
Giacomo Fargion <giacomo.fargion@yahoo.com>
2024-08-08 22:06:03 +0100

    media query on SE bug

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -3,5 +3,5 @@ import '../styles/loading.scss';
 import '../styles/landing.scss';
 import '../styles/projects.scss';
 import '../styles/thefargions.scss';
-import '../styles/contact.scss';
+// import '../styles/contact.scss';
 import '../styles/mastering.scss';
